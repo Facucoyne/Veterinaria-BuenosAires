302a2bccc1b0a04d669fa565871bf48fbf41877e
Facucoyne <hunter.bl12@yahoo.com>
2024-09-28 18:21:53 -0300

    nuevo commit

--- js/main.js
+++ js/main.js
@@ -16,14 +16,14 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click" , () =>{
-    nav.classList.add("visible")
-})
+    abrir.addEventListener("click" , () =>{
+        nav.classList.add("visible")
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+    })
 
+    cerrar.addEventListener("click" , () =>{
+        nav.classList.remove("visible")
+    })
 
 //Servicios
 function Peluqueria(){
